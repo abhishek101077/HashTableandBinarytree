2daffd1f5b2f2dbde659289fdb46a08c3a608a69
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-30 22:51:25 +0530

    The programme file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Hashandtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Binarysearchtree<int> binarySearch = new Binarysearchtree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.GetSize();
+            binarySearch.Display();
+            Console.ReadLine();
+        }
+    }
+}
